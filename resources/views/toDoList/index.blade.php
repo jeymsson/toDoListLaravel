872a6a7c44211029830d3e7b764295628bfbfa7d
Jeymsson <jeymsson@hotmail.com>
2020-04-21 19:54:57 -0300

    Modifing components.

--- resources/views/toDoList/index.blade.php
+++ resources/views/toDoList/index.blade.php
@@ -7,20 +7,29 @@
 @endcomponent
 
 @if (count($base) > 0)
-    @foreach ($base as $item)
-        @isset($item['task'])
-            <br>
-            {{ $item['id'] }} - {{ $item['task'] }}
-            | <a href="{{ route('toDoList.show', $item['id']) }}">Exibir</a>
-            | <a href="{{ route('toDoList.edit', $item['id']) }}">Editar</a>
-            |
-            @component('components.form.delete', ['route' => 'toDoList.destroy', 'id' => $item['id']])
-                Remover
-            @endcomponent
-        @endisset
-    @endforeach
+    @component('components.rel.table', ['head' => $tabela])
+        @foreach ($base as $item)
+            @isset($item['task'])
+
+            <tr>
+                @rel_td(['td' => $item['id']])  @endrel_td
+                @rel_td(['td' => $item['task']])  @endrel_td
+                @rel_td()
+                    <a href="{{ route('toDoList.show', $item['id']) }}">Exibir</a>
+                @endrel_td
+                @rel_td()
+                    <a href="{{ route('toDoList.edit', $item['id']) }}">Editar</a>
+                @endrel_td
+                @rel_td()
+                    @component('components.form.delete', ['route' => 'toDoList.destroy', 'id' => $item['id']])
+                        Remover
+                    @endcomponent
+                @endrel_td
+            </tr>
+            @endisset
+        @endforeach
+    @endcomponent
 @else
     <h4>Nenhum registro encontrado.</h4>
 @endif
-
 @endsection
