0024e7c0e886abb93a37b8eb7b7443561032567c
Jeymsson <jeymsson@hotmail.com>
2020-04-26 01:45:44 -0300

    Adding new fields here

--- resources/views/client/create.blade.php
+++ resources/views/client/create.blade.php
@@ -7,13 +7,26 @@
             @component('components.form.text', [
                 'label' => 'Nome',
                 'id' => 'nome',
-                'back_text' => 'Digite uma nome',
-            ])
+                'back_text' => 'Digite uma nome',])
+            @endcomponent
+            @component('components.form.text', [
+                'label' => 'Idade',
+                'id' => 'idade',
+                'back_text' => 'Digite a idade',])
+            @endcomponent
+            @component('components.form.text', [
+                'label' => 'Endereço',
+                'id' => 'endereco',
+                'back_text' => 'Digite o endereço',])
+            @endcomponent
+            @component('components.form.text', [
+                'label' => 'Email',
+                'id' => 'email',
+                'back_text' => 'Digite um email',])
+            @endcomponent
+            @component('components.form.back', [ 'route' => $btn_back ])
             @endcomponent
             @component('components.form.submit', [ 'value' => 'Cadastrar' ])
-            @endcomponent
-            <br>
-            @component('components.form.back', [ 'route' => $btn_back ])
             @endcomponent
         @endcomponent
     @endcomponent
