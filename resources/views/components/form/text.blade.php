21c3ce687a8f890c800cedf9e4a97f9e1eb96b5f
Jeymsson <jeymsson@hotmail.com>
2020-04-26 02:14:27 -0300

    Update input_text

--- resources/views/components/form/text.blade.php
+++ resources/views/components/form/text.blade.php
@@ -5,7 +5,7 @@
             <label for="{{ $id }}" class="input-group-text col-form-label">{{ $label }}</label>
         </div>
         <div class="col-sm-10">
-            <input type="text" name="{{ $id }}" id="{{ $id }}" class="form-control" placeholder="{{ $back_text ?? '' }}">
+            <input type="text" name="{{ $id }}" id="{{ $id }}" class="form-control" placeholder="{{ $back_text ?? '' }}" value="{{ $value ?? null }}">
         </div>
     </div>
 </div>
