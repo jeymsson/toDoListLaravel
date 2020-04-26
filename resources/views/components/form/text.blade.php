b51f6eb41fa7190c98f3b7dd14cbeb79c244000b
Jeymsson <jeymsson@hotmail.com>
2020-04-26 02:45:25 -0300

    Adding logs

--- resources/views/components/form/text.blade.php
+++ resources/views/components/form/text.blade.php
@@ -5,7 +5,15 @@
             <label for="{{ $id }}" class="input-group-text col-form-label">{{ $label }}</label>
         </div>
         <div class="col-sm-10">
-            <input type="text" name="{{ $id }}" id="{{ $id }}" class="form-control" placeholder="{{ $back_text ?? '' }}" value="{{ $value ?? null }}">
+            <input type="text"
+                name="{{ $id }}" id="{{ $id }}"
+                class="form-control {{ $errors->has($id) ? 'is-invalid' : '' }}"
+                placeholder="{{ $back_text ?? '' }}" value="{{ $value ?? null }}">
+                @if ($errors->has($id))
+                    <div class="invalid-feedback">
+                        {{ $errors->first($id) }}
+                    </div>
+                @endif
         </div>
     </div>
 </div>
