42daad5a169d9bef735dbc15a4db043d8be2de6e
Jeymsson <jeymsson@hotmail.com>
2020-05-13 22:42:05 -0300

    New Models

--- app/Country.php
+++ app/Country.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Country extends Model
 {
-    //
+    public function state()
+    {
+        return $this->hasOne('App\States', 'country_id', 'id');
+    }
+    public function states()
+    {
+        return $this->hasMany('App\States', 'country_id', 'id');
+    }
 }
--- app/States.php
+++ app/States.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class States extends Model
 {
-    //
+    public function country()
+    {
+        return $this->belongsTo('App\Country', 'country_id', 'id');
+    }
 }
